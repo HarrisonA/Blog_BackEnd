0d618b0173bfb217963f8d91530ed48fd9215766
HarrisonA <radiov@gmail.com>
2016-08-10 13:13:46 -0700

    Removed mime type code.  Updated AMS code to current standards.

--- config/initializers/json_api.rb
+++ config/initializers/json_api.rb
@@ -1,8 +1,15 @@
-ActiveModel::Serializer.config.adapter = :json_api
+# Previous implementation that is no longer recommended based on this
+# reference: https://github.com/rails/rails/pull/23712
+#
+# ActiveModel::Serializer.config.adapter = :json_api
+#
+# Mime::Type.register "application/json", :json, %w( text/x-json application/jsonrequest application/vnd.api+json )
 
-Mime::Type.register "application/json", :json, %w( text/x-json application/jsonrequest application/vnd.api+json )
+require 'active_model_serializers/register_jsonapi_renderer'
 
-# this serializes data so it sent from RAILS to the front end (Ember) in the formt:
+ActiveModelSerializers.config.adapter = :json_api
+
+# this serializes data so it sent from RAILS to the front end (Ember) in the format:
 
 # {
   # "data": [
